faf19ab9077102601a03d05cd8007663dce36c1a
Kim Jungin <rlawjddla0203@gmail.com>
2024-09-12 15:08:44 +0900

    Title: 반복 출력하기, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240912/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int count = Integer.parseInt(st.nextToken());
+		while(count-- > 0) {
+			logic();
+		}
+	}
+	private static void logic() {
+		System.out.println("12345^&*()_");
+	}
+}
